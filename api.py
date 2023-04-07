da3b0e5588d6efc2a5b19b52fa41ed23dd4f3760
tarun <55044706+24tarun@users.noreply.github.com>
2023-04-07 16:02:16 +0530

    Delete api.py

--- api.py
+++
@@ -1,7 +0,0 @@
-from binance import Client
-import pandas as pd
-
-api_key='FlA5fSAYb1HHk5ozWNDaljsIot0p0mVlxRgWtOF5kmvJmzt5gRZ1mePKbskadY6U'
-api_secret='spD9RxuIgl6NTwUFFED7jjXgLIMap1s1akywuUgEWFien7LjkmhF7Fr37lRVNlxj'
-client = Client(api_key, api_secret)
-client.get_account()
